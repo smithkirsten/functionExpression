801cf41e279ce7139c978fe439b14fb2a1afc8d0
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-09-26 14:25:40 -0400

    Add player variables and options array

---
+++ rockPaperScissor.js
@@ -0,0 +1,11 @@
+function rockPaperScissors() {
+
+const playerOptions = ["rock", "paper", "scissor"];
+
+const player1 = playerOptions[Math.floor(Math.random() * 3];
+
+const player2 = playerOptions[Math.floor(Math.random() * 3];
+
+
+
+}
