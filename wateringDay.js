e9e1dddfeca9463334520c16a580f3d83cc06487
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-09-26 12:59:25 -0400

    Add intructions and week of month randomizer

--- wateringDay.js
+++ wateringDay.js
@@ -1,7 +1,19 @@
-const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//create a function to track when you should water your plants
+//only water succulents on first Sunday of month
+//water all other plants on Sundays
+//water ferns on Wednesdays also
 
-const index = Math.floor(Math.random() * 7);
+const week = [1, 2, 3, 4, 5];
 
-const dayOfWeek = week[index];
+const day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let index = Math.floor(Math.random() * 7);
+
+const dayOfWeek = day[index];
+
+index = Math.floor(Math.random() * 5);
+
+const weekOfMonth = week[index];
 
 console.log(dayOfWeek);
+console.log(weekOfMonth);
