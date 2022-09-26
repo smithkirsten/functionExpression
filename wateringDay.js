9c9721bd150e1521870ad642f868d3ca7181107c
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-09-26 12:48:12 -0400

    Add day of week randomizer

---
+++ wateringDay.js
@@ -0,0 +1,7 @@
+const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const index = Math.floor(Math.random() * 7);
+
+const dayOfWeek = week[index];
+
+console.log(dayOfWeek);
